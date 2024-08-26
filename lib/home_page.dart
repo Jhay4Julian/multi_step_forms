0cfdc501eaed6f490d8988c79c3124cb2a6dd12b
Julian Koripamo <jkoripamo@gmail.com>
2024-08-26 21:29:16 +0100

    sign up & log in buttons

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:multi_step_forms/components/my_button.dart';
+import 'package:multi_step_forms/pages/login_page.dart';
+import 'package:multi_step_forms/pages/signup_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,17 +12,47 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Icon(
-              Icons.lock,
-              size: 100,
+              Icons.money_off,
+              shadows: [
+                Shadow(
+                    color: Colors.purple.shade100,
+                    blurRadius: 1,
+                    offset: const Offset(3, -3))
+              ],
+              size: 120,
               color: Theme.of(context).primaryColor,
             ),
             const SizedBox(height: 50),
-            MyButton(title: 'Sign Up', onTap: () {}),
-            const SizedBox(height: 20),
-            MyButton(title: 'Log In', onTap: () {}),
+            Column(
+              children: [
+                MyButton(
+                  title: 'Sign Up',
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SignUpPage(),
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(height: 20),
+                MyButton(
+                  title: 'Log In',
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LogInPage(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ],
         ),
       ),
