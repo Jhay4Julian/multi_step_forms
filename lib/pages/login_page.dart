f3ef4de70fcf8ad28443c02cda68e21f4f3e6b76
Julian Koripamo <jkoripamo@gmail.com>
2024-08-28 23:15:54 +0100

    login & success ui

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -12,41 +12,91 @@ class LogInPage extends StatefulWidget {
 class _LogInPageState extends State<LogInPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool notVisible = true;
+  bool loggedIn = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Form(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                'Log In',
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 25,
-                  fontWeight: FontWeight.w600,
-                  letterSpacing: 1,
-                ),
-              ),
-              Column(
-                children: [
-                  MyTextField(
-                    titleController: _emailController,
-                    labelText: 'Email',
-                  ),
-                  const SizedBox(height: 20),
-                  MyTextField(
-                    titleController: _passwordController,
-                    labelText: 'Password',
-                  ),
-                ],
-              ),
-              MyButton(title: 'Login', onTap: () {})
+      body: Center(
+        child: loggedIn ? _buildSuccess() : _buildLoginPage(),
+      ),
+    );
+  }
+
+  Widget _buildSuccess() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.check_circle,
+            shadows: [
+              Shadow(
+                  color: Colors.purple.shade100,
+                  blurRadius: 1,
+                  offset: const Offset(3, -3))
             ],
+            size: 100,
+            color: Theme.of(context).primaryColor,
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLoginPage() {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              'Log In',
+              style: TextStyle(
+                color: Theme.of(context).primaryColor,
+                fontSize: 25,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 1,
+              ),
+            ),
+            Column(
+              children: [
+                // Email TextField
+                MyTextField(
+                  obscureText: false,
+                  titleController: _emailController,
+                  labelText: 'Email',
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 20),
+
+                // Password TextField
+                MyTextField(
+                  titleController: _passwordController,
+                  labelText: 'Password',
+                  obscureText: notVisible,
+                  suffixWidget: InkWell(
+                    onTap: () {
+                      setState(() {
+                        notVisible = !notVisible;
+                      });
+                    },
+                    child: Icon(
+                      notVisible ? Icons.visibility : Icons.visibility_off,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            MyButton(title: 'Login', onTap: () {}),
+          ],
         ),
       ),
     );
